05fe3bac5f609841cd8d8dc3f031b8c580a8ecf7
Michał Łatyński <michal@latynski.pl>
2024-09-05 07:32:32 +0200

    A lot of clean code and fixed warnings about possibility of null variables.

--- SimpleRSA/Program.cs
+++ SimpleRSA/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             RSA.InitializePublicKey();
             Console.WriteLine("Public key - " + RSA.PublicKey);
